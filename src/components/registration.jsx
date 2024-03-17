c0f4f16dbac88961ec38d71b0d3018dfadd81acb
henok256 <henokmengesha1991@gmail.com>
2024-03-17 17:41:48 -0500

    setup api calls

--- src/components/registration.jsx
+++ src/components/registration.jsx
@@ -1,35 +1,67 @@
-import React from "react";
+import axios from "axios";
 
 function Registration(props) {
+  const apiUrl = "http://localhost:8080/signup";
+
   function renderLogin(params) {
     props.handleClick();
   }
+  const registerUser = (event) => {
+    event.preventDefault();
+    /**
+     * event.preventDefault(); // Prevent default form submission
+    const formData = new FormData(event.target);
+    const postData = {
+      username: formData.get("name"),
+      email: formData.get("email"),
+      password: formData.get("password"),
+    };
+    console.log(postData);
+     */
+    const postData = {
+      username: "henok3535",
+      email: "kishor435@yahoo.com",
+      password: "kishoryysy",
+    };
+    console.log(postData);
+
+    axios
+      .post(apiUrl, postData)
+      .then((response) => {
+        // Handle successful response
+        console.log("Response data:", response.data);
+      })
+      .catch((error) => {
+        // Handle error
+        console.error("Error:", error.message);
+      });
+  };
   return (
-    <div className='container'>
+    <div className="container">
       <h1>Create Your IMDb Account</h1>
-      <form action='#' method='post'>
-        <div className='form-group'>
-          <label htmlFor='name'>Name:</label>
-          <input type='text' name='name' id='name' required />
+      <form onSubmit={registerUser}>
+        <div className="form-group">
+          <label htmlFor="name">Name:</label>
+          <input type="text" name="name" id="name" required />
         </div>
-        <div className='form-group'>
-          <label htmlFor='email'>Email:</label>
-          <input type='email' name='email' id='email' required />
+        <div className="form-group">
+          <label htmlFor="email">Email:</label>
+          <input type="email" name="email" id="email" required />
         </div>
-        <div className='form-group'>
-          <label htmlFor='password'>Password:</label>
-          <input type='password' name='password' id='password' required />
+        <div className="form-group">
+          <label htmlFor="password">Password:</label>
+          <input type="password" name="password" id="password" required />
         </div>
-        <div className='form-group'>
-          <label htmlFor='confirm-password'>Confirm Password:</label>
+        <div className="form-group">
+          <label htmlFor="confirm-password">Confirm Password:</label>
           <input
-            type='password'
-            name='confirm-password'
-            id='confirm-password'
+            type="password"
+            name="confirm-password"
+            id="confirm-password"
             required
           />
         </div>
-        <button type='submit'>Register</button>
+        <button type="submit">Register</button>
         <br />
         <a onClick={renderLogin}>Login</a>
       </form>
