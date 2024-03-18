6097816ddc193a57b80e4360331f37e86647333e
henok256 <henokmengesha1991@gmail.com>
2024-03-18 11:06:18 -0500

    added the confirmationPassword variable

--- src/components/registration.jsx
+++ src/components/registration.jsx
@@ -1,7 +1,12 @@
+import { useState } from "react";
 import axios from "axios";
 
 function Registration(props) {
   const apiUrl = "http://localhost:8080/signup";
+  const [username, setUserName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmedPassword, setConfirmedPassword] = useState("");
 
   function renderLogin(params) {
     props.handleClick();
@@ -19,9 +24,10 @@ function Registration(props) {
     console.log(postData);
      */
     const postData = {
-      username: "henok3535",
-      email: "kishor435@yahoo.com",
-      password: "kishoryysy",
+      username: username,
+      email: email,
+      password: password,
+      confirmedPassword: confirmedPassword,
     };
     console.log(postData);
 
@@ -30,6 +36,8 @@ function Registration(props) {
       .then((response) => {
         // Handle successful response
         console.log("Response data:", response.data);
+        localStorage.setItem("token", response.data.token);
+        //we have to redirect the user to home page after successful registration
       })
       .catch((error) => {
         // Handle error
@@ -42,22 +50,45 @@ function Registration(props) {
       <form onSubmit={registerUser}>
         <div className="form-group">
           <label htmlFor="name">Name:</label>
-          <input type="text" name="name" id="name" required />
+          <input
+            type="text"
+            name="username"
+            id="name"
+            value={username}
+            onChange={(e) => setUserName(e.target.value)}
+            required
+          />
         </div>
         <div className="form-group">
           <label htmlFor="email">Email:</label>
-          <input type="email" name="email" id="email" required />
+          <input
+            type="email"
+            name="email"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
         </div>
         <div className="form-group">
           <label htmlFor="password">Password:</label>
-          <input type="password" name="password" id="password" required />
+          <input
+            type="password"
+            name="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
         </div>
         <div className="form-group">
           <label htmlFor="confirm-password">Confirm Password:</label>
           <input
             type="password"
-            name="confirm-password"
+            name="confirmedPassword"
             id="confirm-password"
+            value={confirmedPassword}
+            onChange={(e) => setConfirmedPassword(e.target.value)}
             required
           />
         </div>
