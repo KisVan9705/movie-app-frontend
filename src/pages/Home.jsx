f0badc87fb465a42d35fa0990aaad4963dbe80f2
henok256 <henokmengesha1991@gmail.com>
2024-03-19 15:16:27 -0500

    retrieving movies data from the api

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import Navbar from "../components/navbar";
 import Gallery from "../components/gallery";
+import Slider from "../components/slider";
 import { isAuthenticated } from "../utils/authentication";
 
 function Home() {
   return (
     <>
       <Navbar></Navbar>
-      {isAuthenticated ? <Gallery></Gallery> : <div>Sign In please</div>}
+      {isAuthenticated ? <div> <Slider></Slider><Gallery></Gallery></div> : <div>Sign In please</div>}
     </>
   );
 }
